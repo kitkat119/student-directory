550bb3e9b0aa35d45c9c7308f303976a939c6586
kitkat119 <gprydal@yahoo.co.uk>
2017-06-24 18:03:01 +0000

    saves the data to a file (step 11)

--- directory.rb
+++ directory.rb
@@ -51,6 +51,7 @@ end
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list to students.csv"
     puts "9. Exit."
 end
 
@@ -63,9 +64,11 @@ end
 def process(selection)   
     case selection
         when "1"
-                input_students
+            input_students
         when "2"
-                show_students
+            show_students
+        when "3"
+            save_students
         when "9"
             exit # causes program to terminate
         else
@@ -105,6 +108,18 @@ def print_footer
     else
         puts "Overall, we have #{@students.count} great student"
     end
+end
+
+def save_students
+    # open the file for writing
+    file = File.open("students.csv", "w")
+    # iterate over the students array
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
 end
 
 interactive_menu
