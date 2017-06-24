c2149b2a4853f4a6dcf79e8a8ec9cf751677ffab
kitkat119 <gprydal@yahoo.co.uk>
2017-06-24 20:26:47 +0000

    loads data from csv file

--- directory.rb
+++ directory.rb
@@ -52,6 +52,7 @@ def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv file"
     puts "9. Exit."
 end
 
@@ -69,6 +70,8 @@ def process(selection)
             show_students
         when "3"
             save_students
+        when "4"
+            load_students
         when "9"
             exit # causes program to terminate
         else
@@ -122,8 +125,16 @@ def save_students
     file.close
 end
 
+def load_students
+    file = File.open("students.csv", "r")
+    file.readlines.each do |line|
+        name, cohort = line.chomp.split(',')
+        @students << {name: name, cohort: cohort.to_sym}
+    end
+    file.close
+end
+
 interactive_menu
 
 
 
-.gitignore
